56bb93d6ab15294ea5b1973d25460a88ec4f8a90
yuusuke <xiangzhongyoufu@gmail.com>
2024-04-11 09:22:40 +0900

    first init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const languages = require("./utils/languages");
+
+app.get("/languages", (_, res) => {
+  try {
+    res.status(200);
+    res.json(languages);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT);
---
+++ utils/languages.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1, 
+    language: "C"
+  }, 
+  {
+    id: 2, 
+    language: "C#"
+  }, 
+  {
+    id: 3, 
+    language: "C++"
+  }, 
+  {
+    id: 4, 
+    language: "Dart"
+  }, 
+  {
+    id: 5, 
+    language: "F#"
+  }, 
+  {
+    id: 6, 
+    language: "Go"
+  }, 
+  {
+    id: 7, 
+    language: "Haskell"
+  }, 
+  {
+    id: 8, 
+    language: "JavaScript"
+  }, 
+  {
+    id: 9, 
+    language: "Kotlin"
+  }, 
+  {
+    id: 10, 
+    language: "Lisp"
+  }, 
+];
