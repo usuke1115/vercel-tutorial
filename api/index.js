9299c4994f0175ba123a3f4259a8bf83639b125c
yuusuke <xiangzhongyoufu@gmail.com>
2024-04-12 13:18:42 +0900

    redeploy: 本番環境用の環境変数を定義した

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ const app = express();
 const PORT = 3000;
 const languages = require("../utils/languages");
 
+app.get("/", (req, res) => {
+  res.status(200).send(process.env.ENV_VER);
+})
+
 app.get("/languages", (_, res) => {
   try {
-    res.status(200);
-    res.json(languages);
+    res.status(200).json(languages);
   } catch (err) {
     res.sendStatus(500);
   }
